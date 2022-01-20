8f906a077d1942cb9659a3f938d1e4378fe13a92
RustambekOripov <rustamjon.oripov@upay.uz>
2022-01-20 10:31:41 +0500

    Main.java

---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println(isPalindromeString("abctcba"));
+    }
+    public static boolean isPalindromeString(String input){
+        boolean isPolin=true;
+       if (!input.isEmpty()){
+           if (input.length()%2==0){
+               for (int i = 1; i < input.length()/2; i++) {
+                    char var1 = input.charAt((input.length())/2-i);
+                    char var2 = input.charAt((input.length())/2+i-1);
+                    if (var2!=var1)
+                        isPolin=isPolin&false;
+               }
+           }
+           if (input.length()%2!=0){
+               for (int i = 1; i < input.length()/2; i++) {
+                   char var1 = input.charAt((int)((input.length())/2)-i-1);
+                   char var2 = input.charAt((int)((input.length())/2)+i-1);
+                   if ((var1!=var2))
+                       isPolin=isPolin&false;
+               }
+           }
+       }
+
+       return isPolin;
+    }
+
+}
